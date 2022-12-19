e57b07caf566603ed83684192bf4b7537b0cad34
gloriakamidi <g.kamidi.312@westcliff.edu>
2022-12-19 05:15:26 -0800

    pj2

--- models/Registration.js
+++ models/Registration.js
@@ -16,30 +16,10 @@ const registrationSchema = new mongoose.Schema({
   },
 });
 
-//const RegistrationCol = mongoose.model("Registration", registrationSchema);
+
 module.exports = mongoose.model("Registration", registrationSchema);
 
 
 
 
 
-
-// const express = require("express");
-// const passport = require("passport");
-
-//const passportLocalMongoose = require("passport-local-mongoose");
-
-//const app = express();
-//mongoose.connect('mongodb://localhost/project2app',
-//{ useNewUrlParser: true, useUnifiedTopology: true });
-
-// registrationSchema.plugin(passportLocalMongoose);
-// const Registrations = mongoose.model("Registration", registrationSchema);
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// passport.use(Registrations.createStrategy());
-
-// passport.serializeUser(Registrations.serializeUser());
-// passport.deserializeUser(Registrations.deserializeUser())
